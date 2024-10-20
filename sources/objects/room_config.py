731f6289726d2f973cda1d2f943256c98b0d3a03
PouchyCorp <pouchy.l.acarien@gmail.com>
2024-10-20 18:08:14 +0200

    room bg

--- sources/objects/room_config.py
+++ sources/objects/room_config.py
@@ -1,17 +1,17 @@
 from room import Room
 from coord import Coord 
 from placeable import Placeable
-from pygame import Surface
+from pygame import Surface, image
 
 #R1
-R1 = Room(1,"data/bg_test_1.png")
+R1 = Room(1,image.load("data/bg_test_1.png"))
 R1_stairs = Placeable('R1_stairs', Coord(1,(1600,500)), Surface((100,200)))
 R1.placed.append(R1_stairs)
 R1.blacklist.append(R1_stairs)
 
 
 #R2
-R2 = Room(2,":/data/bg_test_1.png")
+R2 = Room(2,image.load("data/bg_test_1.png"))
 R2_stairs = Placeable('R2_stairs', Coord(1,(1200,300)), Surface((200,300)))
 R2.placed.append(R2_stairs)
 R2.blacklist.append(R2_stairs)
@@ -19,4 +19,4 @@ R2.blacklist.append(R2_stairs)
 
 
 #R3
-R3 = Room(3,":/data/bg_test_1.png")
+R3 = Room(3,image.load("data/bg_test_1.png"))
