999baf713ca66d418f8e48dcfdde147b2b36f3f5
GYtythub <162918388+GYtythub@users.noreply.github.com>
2024-10-22 11:39:07 +0200

    Update chip.py

--- sources/objects/chip.py
+++ sources/objects/chip.py
@@ -6,13 +6,19 @@ class Chip:
         self.price = price
         self.movement : dict = movement
         self.color : list = color
-        self.position = position
+        self.position : Coord = position
         self.heading : str = "right"
 
     def forward(tour):
         for k in range(tour):
           if self.heading == "right":
-            
+              self.position.x += 1
+          if self.heading == "left":
+              self.position.x -= 1        
+          if self.heading == "up":
+              self.position.y += 1
+          if self.heading == "left":
+              self.position.y -= 1               
         
     def paint(self):
         for action, value in self.movement.items():
