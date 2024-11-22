81c149684c630c4844f01600bb1f52410654ed6c
PouchyCorp <pouchy.l.acarien@gmail.com>
2024-11-22 18:53:00 +0100

    Update anim.py

--- sources/objects/anim.py
+++ sources/objects/anim.py
@@ -17,21 +17,21 @@ class Spritesheet:
 class Animation:
     def __init__(self, spritesheet : Spritesheet, line : int, length : int, speed : int = 6) -> None:
         self.spritesheet = spritesheet
-        self.counter : int = 0
+        self.img_index : int = 0
         self.line = line
         self.length = length
         self.speed = speed
         self.__speed_incr = 0
     
     def get_frame(self) -> Surface:
-        if self.counter == self.length:
-            self.counter = 0
+        if self.img_index == self.length:
+            self.img_index = 0
         
         if self.__speed_incr >= self.speed:
-            self.counter += 1
+            self.img_index += 1
             self.__speed_incr = 0 
         else:
             self.__speed_incr += 1
         
-        new_surf = self.spritesheet.get_img((self.counter, self.line))
+        new_surf = self.spritesheet.get_img((self.img_index, self.line))
         return new_surf
