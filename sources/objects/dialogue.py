1b48d2b673395f3a252dbccbfc663c3a45e224fa
Leeeih <bossard.abel@gmail.com>
2024-12-04 21:44:24 +0100

    UwU

--- sources/objects/dialogue.py
+++ sources/objects/dialogue.py
@@ -1,6 +1,6 @@
 from random import *
 import pygame
-from anim import Animation, Spritesheet
+#from anim import Animation, Spritesheet
 #import sprite
 
 pygame.init()
@@ -8,7 +8,6 @@ class Dialogue():
     def __init__(self, fichier):    #nécessite de donner le chemin exacte du fichier
         self.fichier=fichier
         #self.screen=screen    screen
-        self.avatar= pygame.image.load('data\Fond.png').convert_alpha()
         self.texte=[]
         self.number=None
         self.format = pygame.font.SysFont("Arial", 20)
@@ -25,12 +24,16 @@ class Dialogue():
                 mot+=lines[k]
             intermediate.append(mot)
             self.texte.append(intermediate)
+        fich_ouvert.close()
         return self.texte
     
     def load_dialogue(self, number):
         talked=""
         for letter in self.texte[number]:
             talked+=letter
+            print(letter)
+            """if letter =="║":
+                print('bite')"""
         return talked
        
     def show(self, screen, talked):
@@ -43,7 +46,9 @@ class Dialogue():
         self.number=randint(0, (len(self.texte)-1)) 
         return self.number
 
-
+dialoguet2=Dialogue('data\dialogue_t2.txt')
+print(dialoguet2.load_save())
+#dialoguet2.load_dialogue(0)
                         
 #SPRITESHEET_BOT = Spritesheet(pygame.image.load('data/test_robot.png'), (48*6, 48*6))
 #animated = Animation(sprite.SPRITESHEET_BOT, 0, 7)
