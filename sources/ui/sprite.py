20dd3a2c20a1f6ba05caac987ce5f7b6b3cd642e
TiohPAINPERDU <taddeoboisseuil@gmail.com>
2025-01-05 21:15:38 +0100

    ii

--- sources/ui/sprite.py
+++ sources/ui/sprite.py
@@ -128,7 +128,7 @@ SPRITESHEET_BAS_FLIP = anim.Spritesheet(transform.flip(load_image("data/prt_bas.
 
 SPRITESHEET_DOOR_BLINK_FLIP = anim.Spritesheet(transform.flip(load_image("data/prt_anim_blink.png"),False, True), (42*6, 29*6))
 
-PATTERN_LIST = [load_image('data/pattern_storage/pattern_'+str(k+1)+'.png') for k in range(9)]
+#PATTERN_LIST = [load_image('data/pattern_storage/pattern_'+str(k+1)+'.png') for k in range(9)]
 
 BUTTON = load_image('data/button.png')
 
