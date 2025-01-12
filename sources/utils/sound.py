3064f86dc3e8b2f2de228011b302c9dc32753ebb
Leeeih <bossard.abel@gmail.com>
2025-01-12 17:15:24 +0100

    UwU

--- sources/utils/sound.py
+++ sources/utils/sound.py
@@ -1,16 +1,43 @@
 import pygame
 import time
+from random import *
 
 class SoundManager():
-    def __init__(self, name, time = 0):
+    def __init__(self, name, time=0):
         self.name=name
         self.time=time
+        
 
     def played(self):
         PLAYING=pygame.mixer.Sound(self.name)
-        pygame.mixer.music.fadeout(150)
+        pygame.mixer.sound.fadeout(150)
         PLAYING.play(1, self.time*1000)
-        pygame.mixer.music.unload()
+        pygame.mixer.sound.unload()
 
-    def background(self):
-        print('UwU')
+    
+    
+class Sound():
+    def __init__(self):
+        self.noise_blank=['data/sounds/rain.mp3',
+                        'data/sounds/wind.mp3' #a compléter...
+        ]
+        self.robot=['data/sounds/robot.mp3',
+                    'data/sounds/robot1.wav'
+                    'data/sounds/robot2.wav'
+                    'data/sounds/robot3.wav'
+                    'data/sounds/robot4.wav'
+                    'data/sounds/robot5.wav'
+                    'data/sounds/robot6.wav'
+                    'data/sounds/robot7.mp3'
+                    'data/sounds/robots.mp3'
+        ]
+
+    def potential_background(self):
+        a=randint(0,100)
+        if a==69:
+            return randint(0, len(self.noise_blank))
+        return False
+
+    def sound_dialogue(self):
+        a=randint(0, len(self.robot))
+        return SoundManager(self.robot[a]).played()
