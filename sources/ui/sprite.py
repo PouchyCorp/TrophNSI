8c24cf7403a8c49ee17531ec0d002018c901020f
PouchyCorp <pouchy.l.acarien@gmail.com>
2025-01-18 00:16:34 +0100

    babioles

--- sources/ui/sprite.py
+++ sources/ui/sprite.py
@@ -132,7 +132,14 @@ BUTTON = load_image('data/button.png')
 
 WINDOW = load_image('data/bord.png')
 
-<<<<<<< Updated upstream
+#---------------------------------------------
+#       The format for the robot anim is:
+#line 1 - Walk Right
+#line 2 - Walk Left
+#line 3 - Idle Right
+#line 4 - Watch Wall
+#---------------------------------------------
+
 SPRITESHEET_ROBOT_1_PACK = (anim.Spritesheet(load_image('data/robots/robot_1.png'),(24*6,46*6)), [8, 8, 8, 8])
 
 SPRITESHEET_ROBOT_2_PACK = (anim.Spritesheet(load_image('data/robots/robot_2.png'),(31*6,43*6)), [8, 8, 8, 8])
@@ -146,21 +153,6 @@ SPRITESHEET_ROBOT_5_PACK = (anim.Spritesheet(load_image('data/robots/robot_5.png
 SPRITESHEET_ROBOT_MUSIQUE_PACK = (anim.Spritesheet(load_image('data/robots/robot_musique.png'),(48*6,32*6)), [8])
 
 LIST_SPRITESHEET_ROBOT = [SPRITESHEET_ROBOT_1_PACK, SPRITESHEET_ROBOT_2_PACK, SPRITESHEET_ROBOT_3_PACK, SPRITESHEET_ROBOT_4_PACK, SPRITESHEET_ROBOT_5_PACK]
-=======
-#---------------------------------------------
-#       The format for the robot anim is:
-#line 1 - Walk Right
-#line 2 - Walk Left
-#line 3 - Idle Right
-#line 4 - Watch Wall
-#---------------------------------------------
-
-SPRITESHEET_ROBOT_1_PACK = (anim.Spritesheet(load_image('data/robot_3.png'),(27*6,39*6)), [14, 14, 11, 17])
-SPRITESHEET_ROBOT_2_PACK = (anim.Spritesheet(load_image('data/robot_4.png'),(6*8*6,6*8*6)), [8, 8, 8, 8])
-SPRITESHEET_ROBOT_3_PACK = (anim.Spritesheet(load_image('data/robot_5.png'),(6*8*6,6*8*6)), [8, 8, 8, 8])
-
-LIST_SPRITESHEET_ROBOT = [SPRITESHEET_ROBOT_1_PACK, SPRITESHEET_ROBOT_2_PACK, SPRITESHEET_ROBOT_3_PACK]
->>>>>>> Stashed changes
 
 SPRITE_PLANT_1 = load_image("data/plant_2_39x38.png")
 
