884a0df2e69b972d329928c83ae0fd3b0b99c846
GYtythub <tybalt.debruyne@gmail.com>
2025-01-24 18:57:46 +0100

    canva

--- sources/objects/canva.py
+++ sources/objects/canva.py
@@ -13,7 +13,7 @@ class Canva:
         self.pattern_num = 0
 
     def save(self,inv):
-        inv.append(Placeable("painting",self.canva.coord,pg.transform.scale_by(self.canva.surf,0.5)))
+        inv.append(Placeable("painting",self.coord,pg.transform.scale_by(self.surf,0.5)))
         self.surf.fill(pg.Color(0,255,0))
         return self.canva.surf.copy()
 
