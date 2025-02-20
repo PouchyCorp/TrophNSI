296d1a49874744ff1516f013637971bc158dd427
PouchyCorp <pouchy.l.acarien@gmail.com>
2025-02-20 01:01:06 +0100

    cassé

--- sources/objects/canva.py
+++ sources/objects/canva.py
@@ -13,8 +13,99 @@ from objects.particlesspawner import CircleParticleSpawner, ParticleSpawner
 
 COLORS = [(0,0,0), (255,255,255), (255,0,0), (0,0,255), (0,255,0)]
 
+class AnimationHandler:
+    def __init__(self, canva):
+        """Initialize the AnimationHandler with a reference to the Canva object."""
+        self.canva = canva
+
+    def start_anim(self, next_surf):
+        """Start the painting animation with the given surface."""
+        circle_radius = 120
+        step = 25
+        optimal_corner = ceil((circle_radius - (circle_radius * sqrt(2) / 2)))
+        optimal_height = ceil(circle_radius * sqrt(2))
+        width = (self.canva.size[0] - (circle_radius + optimal_corner))
+
+        paint_gun_pos = [-36, 464]
+
+        center = Coord(0, (self.canva.coord.x + paint_gun_pos[0] + circle_radius, self.canva.coord.y + paint_gun_pos[1] + circle_radius))
+        static_particles, aura_particles = self.create_particles(center, circle_radius)
+
+        self.canva.game.particle_spawners[0] += [static_particles, aura_particles]
+        path_stack = self.create_path_stack(width, optimal_height)
+        current_dir = path_stack.pop()
+        active = True
+
+        true_next_surf = self.canva.surf.copy()
+        true_next_surf.blit(next_surf, (0, 0))
+
+        clock = pg.time.Clock()
+        while path_stack or current_dir[1] > 0:
+            clock.tick(60)
+
+            if current_dir[1] <= 0:
+                current_dir = path_stack.pop()
+
+            if current_dir == 'toggle':
+                active = not active
+                static_particles.active = not static_particles.active
+                aura_particles.active = not aura_particles.active
+                current_dir = path_stack.pop()
+
+            if active:
+                self.canva.surf.blit(self.canva.get_round_mask(next_surf, tuple(paint_gun_pos), circle_radius), paint_gun_pos)
+
+            current_dir[1] -= step
+
+            next_step = step + current_dir[1] if current_dir[1] < 0 else step
+
+            self.update_paint_gun_pos(current_dir[0], paint_gun_pos, next_step)
+
+            mouse_pos = Coord(0, pg.mouse.get_pos())
+            center.xy = (self.canva.coord.x + paint_gun_pos[0] + circle_radius, self.canva.coord.y + paint_gun_pos[1] + circle_radius)
+
+            self.canva.game.update(mouse_pos)
+            self.canva.game.draw(mouse_pos)
+            self.canva.blit_arms(self.canva.game.win)
+
+            self.canva.arm['angle'], self.canva.forearm['angle'] = inverse_kinematics(center.xy, self.canva.arm_root, self.canva.arm['len'], self.canva.forearm['len'])
+
+            pg.display.flip()
+            print(f"relative : {paint_gun_pos}")
+
+        self.canva.surf = true_next_surf
+        static_particles.active = False
+        aura_particles.active = False
+        self.canva.game.timer.create_timer(5, self.canva.game.particle_spawners[0].remove, arguments=[static_particles])
+        self.canva.game.timer.create_timer(5, self.canva.game.particle_spawners[0].remove, arguments=[aura_particles])
+
+    def create_particles(self, center, circle_radius):
+        """Create and return static and aura particle spawners."""
+        static_particles = CircleParticleSpawner(center, circle_radius, pg.Vector2(0, 0), self.canva.current_color, 600, density=50, dir_randomness=0, radius=(10, 20))
+        aura_particles = ParticleSpawner(center, pg.Vector2(0, 0), self.canva.current_color, 60, dir_randomness=2)
+        return static_particles, aura_particles
+
+    def create_path_stack(self, width, optimal_height):
+        """Create and return the path stack for the painting animation."""
+        return [["L", 100], 'toggle', ["L", width], ["D", optimal_height], ["R", width], ["D", optimal_height],
+                ["L", width], ["D", optimal_height], ["R", width], ["D", optimal_height],
+                ["L", width], ["D", optimal_height], ["R", width], 'toggle', ["R", 100], ["U", 350]]
+
+    def update_paint_gun_pos(self, direction, paint_gun_pos, step):
+        """Update the paint gun position based on the current direction and step."""
+        match direction:
+            case "R":
+                paint_gun_pos[0] += step
+            case "L":
+                paint_gun_pos[0] -= step
+            case "D":
+                paint_gun_pos[1] += step
+            case "U":
+                paint_gun_pos[1] -= step
+
 class Canva:
     def __init__(self, coord : Coord, game): 
+        """Initialize the Canva object with its properties and UI elements."""
         self.coord = coord
 
         self.size = (672,1020)
@@ -26,7 +117,7 @@ class Canva:
         self.rect.x, self.rect.y = self.coord.xy
         
         self.name_input = InputBox(1446, 210, 100, 50)
-        self.confirm_button = Button((1556, 210), self.attempt_save, whiten(NO_BUTTON), NO_BUTTON) # Int is a callable placeholder.
+        self.confirm_button = Button((1556, 210), self.attempt_save, whiten(NO_BUTTON), NO_BUTTON)
         self.paint_button = Button((1556, 310), self.start_painting, whiten(YES_BUTTON), YES_BUTTON)
 
         self.color_buttons = self.init_color_buttons()
@@ -41,17 +132,21 @@ class Canva:
         self.total_price = 0
         self.total_beauty = 0
 
-        self.arm_root = (552, 469)
-        self.default_target = (572, 469)
+        self.arm_root = (522, 469)
+        self.default_target = (-136+self.coord.x, 314+self.coord.y)
         self.arm = {'surf' : ARM.copy(), 'len' : ARM.copy().get_size()[0], 'angle' : 0}
         self.forearm = {'surf' : ARM.copy(), 'len' : ARM.copy().get_size()[0], 'angle' : 0}
 
         self.current_color = (255,0,0)
 
+        self.animation_handler = AnimationHandler(self)
+
     def change_color(self, color):
+        """Change the current color used for painting."""
         self.current_color = color
 
     def init_color_buttons(self):
+        """Initialize the color selection buttons."""
         buttons : list[Button] = []
         size = (120,120)
         x = 12
@@ -64,6 +159,7 @@ class Canva:
         return buttons
 
     def get_placeable(self) -> Placeable:
+        """Create and return a Placeable object from the current canvas."""
         scaled_surf = pg.transform.scale_by(self.surf.copy(),0.5).convert()
         FRAME_PAINTING.blit(scaled_surf,(12,12))
         placeable = Placeable(self.name, self.coord, FRAME_PAINTING.copy(), beauty=self.total_beauty, tag="decoration")
@@ -71,6 +167,7 @@ class Canva:
         return placeable
     
     def check_price(self, price):
+        """Check if the player has enough money to paint and deduct the price if possible."""
         if self.game.money - price >= 0:
             self.game.money -= price
             return True
@@ -80,19 +177,23 @@ class Canva:
             return False
 
     def get_price(self):
+        """Calculate and return the total price of all placed patterns."""
         self.total_price = 0
         for pattern in self.placed_patterns:
             self.total_price += pattern.price
         return self.total_price
 
     def add_to_beauty(self, patterns : list[Pattern]):
+        """Add the beauty value of the given patterns to the total beauty."""
         for pattern in patterns:
             self.total_beauty += pattern.beauty
     
     def reset(self):
+        """Reset the canvas to its initial state."""
         self.__init__(self.coord, self.game)
     
     def get_round_mask(self, surface : pg.Surface, xy : tuple, circle_radius):
+        """Create and return a circular mask for the given surface."""
         circle_surf = pg.Surface((circle_radius * 2, circle_radius * 2), pg.SRCALPHA)
         pg.draw.circle(circle_surf, (255, 255, 255, 255), (circle_radius, circle_radius), circle_radius)
 
@@ -100,6 +201,7 @@ class Canva:
         return circle_surf
     
     def get_next_surf(self):
+        """Generate and return the next surface to be painted."""
         next_surf = pg.Surface(self.size)
         next_surf = next_surf.convert_alpha()
         next_surf.fill((0,0,0,0))
@@ -111,102 +213,26 @@ class Canva:
         next_surf.fill(self.current_color+tuple([0]), special_flags=pg.BLEND_RGBA_MAX)
         return next_surf
     
-    def start_anim(self, next_surf):
-
-        circle_radius = 120
-        step = 25
-        optimal_corner = ceil((circle_radius-(circle_radius*sqrt(2)/2)))
-        optimal_height = ceil(circle_radius*sqrt(2))
-        width = (self.size[0]-(circle_radius+optimal_corner))
-
-        paint_gun_pos = [-optimal_corner, -optimal_corner]
-
-        center = Coord(0,(self.coord.x+paint_gun_pos[0]+circle_radius, self.coord.y+paint_gun_pos[1]+circle_radius))
-        static_particles = CircleParticleSpawner(center, circle_radius, pg.Vector2(0,0), self.current_color, 600, density=50, dir_randomness=0, radius=(10,20))
-        aura_particles = ParticleSpawner(center, pg.Vector2(0,0), self.current_color, 60, dir_randomness=2)
-
-
-        self.game.particle_spawners[0] += [static_particles, aura_particles]
-        #order is inversed as it is a stack
-        path_stack = [["R",width], ["D",optimal_height],["L",width],["D",optimal_height],
-                      ["R",width],["D",optimal_height],["L",width], ["D",optimal_height],
-                      ["R",width],["D",optimal_height],["L",width], ["D",optimal_height], ["R",width]]            
-        current_dir = path_stack.pop()
-        active = True
-        static_particles.active = True
-        aura_particles.active = True
-        
-        true_next_surf = self.surf.copy()
-        true_next_surf.blit(next_surf, (0,0))
-
-        clock = pg.time.Clock()
-        while path_stack:
-            clock.tick(60)
-
-            if current_dir == 'toggle':
-                active = not active
-                static_particles.active = not static_particles.active
-                aura_particles.active = not aura_particles.active
-                current_dir = path_stack.pop()
-
-            if current_dir[1] <= 0:
-                current_dir = path_stack.pop()
-            
-            if active:
-                self.surf.blit(self.get_round_mask(next_surf, tuple(paint_gun_pos), circle_radius), paint_gun_pos)
-
-            current_dir[1] -= step
-
-            if current_dir[1] < 0:
-                next_step = step + current_dir[1]
-            else:
-                next_step = step
-
-            match current_dir[0]:
-                case "R":
-                    paint_gun_pos[0] += next_step
-                case "L":
-                    paint_gun_pos[0] -= next_step
-                case "D":
-                    paint_gun_pos[1] += next_step
-                case "U":
-                    paint_gun_pos[1] -= next_step
-            
-            mouse_pos = Coord(0,pg.mouse.get_pos())
-            center.xy = (self.coord.x+paint_gun_pos[0]+circle_radius, self.coord.y+paint_gun_pos[1]+circle_radius)
-
-            self.game.update(mouse_pos)
-            self.game.draw(mouse_pos)
-            self.blit_arms(self.game.win)
-    
-            self.arm['angle'], self.forearm['angle'] = inverse_kinematics(center.xy, self.arm_root, self.arm['len'], self.forearm['len'])
-
-            pg.display.flip()
-        
-        self.surf = true_next_surf
-        static_particles.active = False
-        aura_particles.active = False
-        self.game.timer.create_timer(5, self.game.particle_spawners[0].remove, arguments=[static_particles])
-        self.game.timer.create_timer(5, self.game.particle_spawners[0].remove, arguments=[aura_particles])
-
     def start_painting(self):
+        """Start the painting process if the player has enough money."""
         if self.check_price(self.get_price()):
-            self.start_anim(self.get_next_surf())
+            self.animation_handler.start_anim(self.get_next_surf())
             self.add_to_beauty(self.placed_patterns)
     
     def draw_pattern(self, surf, pattern : Pattern):
-        """Imprints the pattern on the canva."""
+        """Imprint the pattern on the given surface."""
         relative_pos = (pattern.rect.x - self.coord.x,
                         pattern.rect.y - self.coord.y)
 
         surf.blit(pattern.get_effect(), relative_pos)
 
     def place_pattern(self, pattern : Pattern):
-        """Place a moveable pattern."""
+        """Place a moveable pattern on the canvas."""
         self.placed_patterns.append(pattern)
         self.get_price()
 
     def hold_pattern(self, pattern):
+        """Hold a pattern for moving."""
         self.placed_patterns.remove(pattern)
         self.get_price()
         self.holded_pattern = pattern
@@ -214,6 +240,7 @@ class Canva:
         self.game.sound_manager.items.play()
     
     def drop_pattern(self, pos):
+        """Drop the held pattern at the given position."""
         if self.rect.collidepoint(pos):
             self.holded_pattern.rect.center = pos
             self.place_pattern(self.holded_pattern)
@@ -223,10 +250,12 @@ class Canva:
         self.holded_pattern = None
 
     def attempt_save(self):
+        """Attempt to save the current canvas."""
         self.name = self.name_input.text
         self.game.confirmation_popups.append(ConfirmationPopup(self.game.win, "are you sure you want to save the canva ?", self.game.save_canva))
 
     def draw(self, win : pg.Surface):
+        """Draw the canvas and its elements on the given window surface."""
         win.blit(self.surf, self.coord.xy)
 
         for placed_pattern in self.placed_patterns:
@@ -246,6 +275,7 @@ class Canva:
         self.blit_arms(win)
     
     def blit_arms(self, win):
+        """Draw the robotic arms on the given window surface."""
         rotated_surf, rect = point_rotate(self.arm['surf'], self.arm_root, (5,5), -self.arm['angle'])
         relative_arm_vector = pg.Vector2(self.arm['len'], 0)
         rotated_arm_vector = relative_arm_vector.rotate(self.arm['angle'])
@@ -264,6 +294,7 @@ class Canva:
         win.blit(SPRAYER, sprayer_rect)
 
     def handle_event(self, event):
+        """Handle user input events."""
         mouse_pos = pg.mouse.get_pos()
 
         self.name_input.handle_event(event)
@@ -287,4 +318,3 @@ class Canva:
                 self.holded_pattern.rect.center = mouse_pos
 
         return False
-            
