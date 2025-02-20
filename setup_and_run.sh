7846425bd3cd4b27c4220f8e429d73dc7e24184e
PouchyCorp <pouchy.l.acarien@gmail.com>
2025-02-20 21:49:52 +0100

    skibidi

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -4,15 +4,12 @@
 echo "Creating a virtual environment..."
 python3 -m venv creative_core_venv
 
-# Activate the virtual environment
-source creative_core_venv/bin/activate
-
 # Install dependencies
 echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
+creative_core_venv/bin/pip install --upgrade pip
+creative_core_venv/bin/pip install -r requirements.txt
 
 # Launch the game
 echo "Launching the game..."
 echo "Credits : PouchyCorp, Tioh, Ytyt, Leih"
-python3 sources/main.py
+creative_core_venv/bin/python3 sources/main.py
