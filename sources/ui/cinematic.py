420b1653a797c44b6b9b63520b1403d981d3c039
PouchyCorp <pouchy.l.acarien@gmail.com>
2025-03-05 19:51:35 +0100

    commentaires

--- sources/ui/cinematic.py
+++ sources/ui/cinematic.py
@@ -30,92 +30,122 @@ class CinematicPlayer:
         if event.type == pg.MOUSEBUTTONDOWN:
             return self.dialogue.click_interaction()
             
-    def play_anim(self, game : 'Game'):
+    def __play_anim(self, game : 'Game'):
+        # Define the size of the black bands at the top and bottom of the screen
         band_size = (game.win.get_width(), 140)
         black_band = pg.Surface(band_size)
         clock = pg.time.Clock()
+        
+        # Loop until the animation is finished or the cinematic is marked as finished
         while not self.anim.is_finished() and not self.is_finished:
-            clock.tick(60)
+            clock.tick(60)  # Cap the frame rate at 60 FPS
             for event in pg.event.get():
+                # Handle status events like quitting or pressing escape
                 self.get_status_event(event, game)
 
+            # Get the current frame of the animation
             self.cutscene_surf = self.anim.get_frame()
-            self.cutscene_surf.blit(black_band, (0,0))
-            self.cutscene_surf.blit(black_band, (0,1080-band_size[1]))
+            # Draw black bands at the top and bottom of the screen
+            self.cutscene_surf.blit(black_band, (0, 0))
+            self.cutscene_surf.blit(black_band, (0, 1080 - band_size[1]))
             
+            # Draw the background and the current frame of the animation
             game.draw_background()
-            game.win.blit(self.cutscene_surf, (0,0))
+            game.win.blit(self.cutscene_surf, (0, 0))
             pg.display.flip()
         
+        # Convert the final frame to grayscale
         self.cutscene_surf = pg.transform.grayscale(self.cutscene_surf)
     
-    def play_dialogue(self, game : 'Game'):
+    def __play_dialogue(self, game : 'Game'):
+        """Play the dialogue sequence."""
         clock = pg.time.Clock()
         finised_reading = False
         while not finised_reading and not self.is_finished:
+            # Cap the frame rate at 60 FPS
             clock.tick(60)
             for event in pg.event.get():
+                # Handle status events like quitting or pressing escape
                 self.get_status_event(event, game)
+                # Handle dialogue events like mouse clicks and check if finished reading
                 finised_reading = self.get_dialogue_event(event)
             
+            # Update the dialogue state
             self.dialogue.update()
+            # Draw the background and the current state of the dialogue
             game.draw_background()
             game.win.blit(self.cutscene_surf, (0,0))
             self.dialogue.draw(game.win)
+            # Update the display
             pg.display.flip()
     
-    def play_introspection_dialogue(self, game : 'Game'):
+    def __play_introspection_dialogue(self, game : 'Game'):
+        """Play the introspection dialogue."""
         clock = pg.time.Clock()
         while not self.dialogue.selected_dialogue.is_on_last_part() and not self.is_finished:
             clock.tick(60)
             for event in pg.event.get():
+                # Handle status events like quitting or pressing escape
                 self.get_status_event(event, game)
+                # Handle dialogue events like mouse clicks
                 self.get_dialogue_event(event)
             
+            # Update the dialogue state
             self.dialogue.update()
-            game.draw(Coord(0,(0,0)))
+            # Draw the background and the current state of the introspection dialogue
+            game.draw(Coord(0, (0, 0)))
             self.dialogue.draw(game.win)
+            # Update the display
             pg.display.flip()
 
-    def play_transition(self, game : 'Game'):
-        """Play a easy fade out transition"""
+    def __play_transition(self, game : 'Game'):
+        """Play a simple fade out transition"""
         clock = pg.time.Clock()
         mask = pg.Surface((game.win.get_width(), game.win.get_height()))
-        mask.fill((0,0,0))
+        mask.fill((0, 0, 0))
         incr = 0
-        step_count = 2*60
+        step_count = 2 * 60  # Number of steps for the transition
+
         while incr < pi and not self.is_finished:
             clock.tick(60)
-            incr += pi/step_count
+            incr += pi / step_count  # Increment the angle for the sine function
 
             for event in pg.event.get():
                 self.get_status_event(event, game)
 
-            if incr < pi/2:
+            if incr < pi / 2:
+                # First half of the transition: draw the current cutscene and dialogue
                 game.draw_background()
-                game.win.blit(self.cutscene_surf, (0,0))
+                game.win.blit(self.cutscene_surf, (0, 0))
                 self.dialogue.draw(game.win)
             else:
-                game.draw(Coord(0,(0,0))) 
-            
-            mask.set_alpha(sin(incr)*255)
-            print(mask.get_alpha())
-            game.win.blit(mask, (0,0))
+                # Second half of the transition: draw the introspection dialogue
+                game.draw(Coord(0, (0, 0)))
+
+            # Use the sine function to create a smooth fade out effect
+            # sin(incr) varies from 0 to 1 as incr goes from 0 to pi/2, and from 1 to 0 as incr goes from pi/2 to pi
+            mask.set_alpha(sin(incr) * 255)
+            game.win.blit(mask, (0, 0))
 
             pg.display.flip()
 
     def play(self, game : 'Game'):
-        self.play_anim(game)
+        # Play the animation sequence
+        self.__play_anim(game)
 
+        # If there is a dialogue name, play the dialogue sequence
         if self.dialogue_name:
             self.dialogue.special_dialogue(self.dialogue_name)
-            self.play_dialogue(game)
+            self.__play_dialogue(game)
 
-        self.play_transition(game)
+        # Play the transition sequence
+        self.__play_transition(game)
 
+        # If there is an introspection dialogue, play the introspection dialogue sequence
         if self.introspection_dialogue:
             self.dialogue.special_dialogue(self.introspection_dialogue)
-            self.play_introspection_dialogue(game)
+            self.__play_introspection_dialogue(game)
 
+        # Mark the cinematic as finished
         self.is_finished = True
     
